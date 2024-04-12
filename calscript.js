689ca16cc71fd16f95bf296f97b9c32faa44c6c8
gitdeepshikha097 <100943544+gitdeepshikha097@users.noreply.github.com>
2024-04-12 14:12:00 +0530

    calculator project

---
+++ calscript.js
@@ -0,0 +1,26 @@
+let display=document.getElementById('inputbox');
+let buttons=document.querySelectorAll('button');
+
+
+let buttonsArray = Array.from(buttons);
+let string='';
+
+buttonsArray.forEach(btn => {
+	btn.addEventListener('click',(e) => {
+        if(e.target.innerHTML=='DEL'){
+		string=string.substring(0,string.length-1);
+		display.value=string;
+	   }else if(e.target.innerHTML=='AC'){
+           string=' ';
+		 display.value=string;
+	   }else if(e.target.innerHTML=='='){
+           string=eval(string);
+		 display.value=string;
+	   }
+	   else{
+		string+=e.target.innerHTML;
+		display.value=string;
+	   }
+		
+	});
+});
